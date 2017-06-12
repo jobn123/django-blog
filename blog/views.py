ecf9411457378b39a4a940b8f77d7fb2079e1d4f
ZhouBin <jobn_joy@163.com>
2017-06-12 14:26:35 +0800

    perfect blog

--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,48 @@ from django.http import HttpResponse
 from blog.models import Atricle
 
 # Create your views here.
-def Home(request):
+def home(request):
   post_list = Atricle.objects.all()  # 获取全部article 对象
   return render(request, 'home.html', {'post_list': post_list})
   # return HttpResponse("Hello World, Django")
 
-def detail(request, my_args):
-  return HttpResponse("You're looking at my_args %s." % my_args)
+def detail(request, id):
+  try:
+    post = Atricle.objects.get(id=str(id))
+  except Atricle.DoseNotExist:
+    raise Http404
+  
+  return render(request, 'post.html', {'post': post})
+
+def archives(request):
+  try:
+    post_list = Atricle.objects.all()
+  except Atricle.DoseNotExist:
+    raise Http404
+  return render(request, 'archives.html', {'post_list' : post_list, 
+                                            'error' : False})
+                                  
+def about_me(request) :
+    return render(request, 'aboutme.html')
+
+def search_tag(request, tag) :
+    try:
+        post_list = Atricle.objects.filter(category__iexact = tag) #contains
+    except Article.DoesNotExist :
+        raise Http404
+    return render(request, 'tag.html', {'post_list' : post_list})
+
+def blog_search(request):
+    if 's' in request.GET:
+        s = request.GET['s']
+        if not s:
+            return render(request,'home.html')
+        else:
+            post_list = Atricle.objects.filter(title__icontains = s)
+            if len(post_list) == 0 :
+                return render(request,'archives.html', {'post_list' : post_list,
+                                                    'error' : True})
+            else :
+                return render(request,'archives.html', {'post_list' : post_list,
+                                                    'error' : False})
+    return redirect('/')
--- django_blog/urls.py
+++ django_blog/urls.py
@@ -19,6 +19,10 @@ from blog import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.Home),
-    url(r'^(?P<my_args>\d+)/$', views.detail, name='detail'),
+    url(r'^$', views.home, name='home'),
+    url(r'^(?P<id>\d+)/$', views.detail, name='detail'),
+    url(r'^archives/$', views.archives, name = 'archives'),
+    url(r'^aboutme/$', views.about_me, name = 'aboutme'),
+    url(r'^tag(?P<tag>\w+)/$', views.search_tag, name = 'search_tag'),
+    url(r'^search/$', views.blog_search, name = 'search'),
 ]
